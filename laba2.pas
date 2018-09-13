73e015933d9ac1f2670a3544cd7da0a23afc1a92
Pasha Govnyasha <pasha19682000d@gmail.com>
2018-09-13 20:59:22 +0300

    2

---
+++ laba2.pas
@@ -0,0 +1,26 @@
+﻿// запустишь с оболочкой - 你花 работать не будет (shift+F9)
+program laba;
+uses crt;
+var b:byte;
+w:word;
+s:shortint;
+i:integer;
+l:longint;
+r:real;
+bb:boolean;
+c:char;
+begin
+bb:=true;
+writeln('Enter byte \n');
+readln(b);
+writeln('sizeof=',sizeof(byte));
+writeln('ord=',ord(b));
+writeln('pred=',pred(b));
+writeln('succ=',succ(b));
+writeln('abs=',abs(b));
+writeln('sqr=',sqr(b));
+writeln('sqrt=',sqrt(b));
+writeln('sin=',sin(b));
+writeln('cos=',cos(b));
+writeln('ln=',ln(b));
+end.
