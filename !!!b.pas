0f57d30ca39bf20b1f870decc15821117f4ab821
Pasha Govnyasha <pasha19682000d@gmail.com>
2018-10-25 17:21:37 +0300

    2 фактор

---
+++ !!!b.pas
@@ -0,0 +1,31 @@
+program doublefact;
+uses crt;
+var fac1,fac2,f,i,n:integer;
+ begin
+ writeln('enter n');
+ readln(n);
+ fac1:=1;
+ if n mod 2 = 0 then
+ 	begin
+ 		i:=0;
+ 		while i<n do
+ 		begin
+ 			i:=i+2;
+ 			fac1:=fac1*i;
+      		f:=fac1;
+      		
+      	end
+ 	end;
+if n mod 2 = 1 then
+ 	begin
+ 		i:=1;
+ 		while i<n do
+ 		begin
+ 			i:=i+2;
+ 			fac1:=fac1*i;
+      		f:=fac1;
+      		
+      	end
+ 	end;
+ write(f)
+end.
