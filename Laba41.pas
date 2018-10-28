179a23e6dd591a0552e64a465451e461734921ab
Pasha Govnyasha <pasha19682000d@gmail.com>
2018-10-28 21:19:49 +0300

    Update Laba41.pas

---
+++ Laba41.pas
@@ -0,0 +1,30 @@
+﻿program laba41;
+//Портянка задаются 2 массива Х и У ,складіваем их ячейки и у тот, у кого сумма меньше-обнуляем
+var 
+  X,Y,zero:array [1..10] of real;
+ { Y:array [1..10] of integer;
+  zero:array[1..10] of integer;}
+  sum1,sum2:real;
+  count:integer;
+begin
+count:=1;
+  while count <= 10 do 
+    begin
+      zero[count]:=0;
+      X[count]:= random(1000);
+      Y[count]:= random(1000);
+      sum1:=X[count]+sum1;
+      sum2:=Y[count]+sum2;
+      count:=count+1;
+    end;
+    writeln(sum1,' ',sum2);
+    if sum1>sum2 then 
+      begin
+        Y:=zero;
+      end;
+    if sum1<sum2 then 
+      begin
+        X:=zero;
+      end;
+    writeln(X,Y)
+end.
