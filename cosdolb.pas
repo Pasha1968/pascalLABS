43ac610d7a32d521ebd586085f2d3846edf56629
Pasha Govnyasha <pasha19682000d@gmail.com>
2018-10-30 21:22:03 +0300

    Update cosdolb.pas

---
+++ cosdolb.pas
@@ -0,0 +1,28 @@
+﻿program cosprib;
+//Портянка
+
+Var
+ res,fact,x,eps:real;
+ i,n,m,j:integer;
+begin
+  j:=1;
+  m:=1;
+  res:=1;
+  write('x,n: ');
+  readln(x,n);
+  while i <> n do
+  begin
+    fact:=1;
+    eps:=1;
+    while j < i*2 do
+    begin
+       fact:=fact*j;
+       eps:=eps*x;
+       j:=j+1;
+    end;
+    m:=-m;
+    res:=res+(m*(eps/fact));
+    i:=i+1;
+  end;
+writeln('cos:=',res);
+end.
