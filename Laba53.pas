78001135dd02f46e3236cbf41f1d7b8a48ca561d
Pasha Govnyasha <pasha19682000d@gmail.com>
2018-11-22 15:54:33 +0200

    Update Laba53.pas

---
+++ Laba53.pas
@@ -0,0 +1,30 @@
+﻿program laba53;
+var
+  x:real;
+
+function Sinus(x:real):real;
+begin
+  Sinus:=sin(x);
+end;
+function Cosinus(x:real):real;
+begin
+  Cosinus:=cos(x);
+end;
+procedure Print();
+begin
+  writeln('cosx/sinx=',Cosinus/Sinus);
+end;
+
+procedure Print2();
+begin
+  writeln('cosx*sinx=',cos(x)*sin(x));
+end;
+
+begin
+  writeln('vvedi x');
+   read(x);
+  writeln(Sinus(x));
+  writeln(Cosinus(x));
+  Print;
+  Print2;
+end.
