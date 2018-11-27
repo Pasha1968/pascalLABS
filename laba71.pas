1ec91b7af0135fab5697b298538662074dc276a0
Pasha Govnyasha <pasha19682000d@gmail.com>
2018-11-27 15:42:14 +0200

    Create laba71.pas

---
+++ laba71.pas
@@ -0,0 +1,25 @@
+﻿const m = 20;
+      n = 20;
+var f:text;
+    i,j: integer;
+    nmax:integer;
+    arr:array[1..m,1..n] of integer;
+begin
+  assign(f,'massiv.txt');
+  rewrite(f);
+  writeln('kakoi massiv sohranim v fail (do 20x20)');
+  readln(nmax);
+  for i := 1 to nmax do
+    begin
+      for j := 1 to nmax do
+        begin
+          arr[i,j]:=random(10);
+          write(arr[i,j]:2);
+          Write (f, arr[i,j]:2);
+        end;
+      Writeln (f);
+      writeln;
+    end;
+  writeln ('------------');
+  close(f);
+end.
